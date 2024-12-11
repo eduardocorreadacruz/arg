70a7049acaa7b4255eb755220788f3ec9b199c42
eduardocorreadacruz <eduardo_c_cruz@estudante.sesisenai.org.br>
2024-12-11 03:33:58 -0300

    0.4

--- login.js
+++ login.js
@@ -1,1 +1,25 @@
-function toggleForm(formType){document.getElementById('usuarioForm').classList.remove('active');document.getElementById('funcionarioForm').classList.remove('active');if(formType === 'usuario') {document.getElementById('usuarioForm').classList.add('active');}else {document.getElementById('funcionarioForm').classList.add('active');}}document.getElementById("entrarFuncionario").addEventListener("click", function() {const codigoAcesso = document.getElementById("codigoAcesso").value;const codigoCorreto = "72658711034";if(codigoAcesso === codigoCorreto){window.location.href = "paginaFuncionario.html";}else {const errorMessage = document.getElementById("error-message");errorMessage.innerText = "Código de acesso incorreto. Tente novamente.";errorMessage.style.display = "block";}})
+function toggleForm(formType) {  
+    document.getElementById('usuarioForm').classList.remove('active');  
+    document.getElementById('funcionarioForm').classList.remove('active');  
+    
+    if (formType === 'usuario') {  
+        document.getElementById('usuarioForm').classList.add('active');  
+    } else {  
+        document.getElementById('funcionarioForm').classList.add('active');  
+    }  
+
+    
+}  
+
+document.getElementById("entrarFuncionario").addEventListener("click", function() {
+    const codigoAcesso = document.getElementById("codigoAcesso").value;
+    const codigoCorreto = "72658711034";
+
+    if (codigoAcesso === codigoCorreto) {
+        window.location.href = "paginaFuncionario.html";
+    } else {
+        const errorMessage = document.getElementById("error-message");
+        errorMessage.innerText = "Código de acesso incorreto. Tente novamente.";
+        errorMessage.style.display = "block";
+    }
+})
